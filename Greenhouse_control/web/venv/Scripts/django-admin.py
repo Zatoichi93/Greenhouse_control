e054b7f79d02b4cdf09aed20cba3536409bac01b
Zatoichi93 <valerio.longo.93@gmail.com>
2018-10-21 20:04:03 +0200

    First commit

---
+++ Greenhouse_control/web/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\zatoi\PycharmProjects\Greenhouse_control\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
